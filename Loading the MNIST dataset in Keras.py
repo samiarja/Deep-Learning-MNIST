124fc141e24c0342e1bc7f83c83b00fcf4ac4088
Sami Arja <sami18040571@outlook.com>
2018-03-06 21:34:06 +1100

    Add files via upload

---
+++ Loading the MNIST dataset in Keras.py
@@ -0,0 +1,12 @@
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+(X_train, y_train), (X_test, y_test)=mnist.load_data()
+plt.subplot(221)
+plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
+plt.subplot(222)
+plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
+plt.subplot(223)
+plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
+plt.subplot(224)
+plt.imshow(X_train[7], cmap=plt.get_cmap('gray'))
+plt.show()
